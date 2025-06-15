3ed0a65ca08ebf077532e09ee59337c9a327bb8f
jamesdanfajardo <jamesdanfajardo@gmail.com>
2025-06-15 14:43:00 +0800

    initial commit

---
+++ src/main/java/com/jamesdan/challenges/Application.java
@@ -0,0 +1,7 @@
+package com.jamesdan.challenges;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello from the Java Challenges Collection application!");
+    }
+}
